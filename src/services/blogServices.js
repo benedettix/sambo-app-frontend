d930b5ca2c6f87d2d832157cce47f23cc1e6573d
Luka Benedetti <benedetiluka@gmail.com>
2022-09-23 18:37:49 +0200

    new coim

--- src/services/blogServices.js
+++ src/services/blogServices.js
@@ -2,69 +2,86 @@ import axios from "axios";
 
 class blogServices {
   static getBlogs() {
-    return axios.get("http://localhost:5000/blogovi");
+    return axios.get("https://sambo-klub-neretva.herokuapp.com/blogovi");
   }
   static getBlogByid(id) {
-    return axios.get(`http://localhost:5000/blogovi/${id}`);
+    return axios.get(`https://sambo-klub-neretva.herokuapp.com/blogovi/${id}`);
   }
   static uploadBlog(body) {
-    return axios.post(`http://localhost:5000/blogovi/dodajblog/uspesno`, body);
+    return axios.post(
+      `https://sambo-klub-neretva.herokuapp.com/blogovi/dodajblog/uspesno`,
+      body
+    );
   }
   static uploadComment(id, body) {
     return axios.post(
-      `http://localhost:5000/blogovi/${id}/objavikomentar`,
+      `https://sambo-klub-neretva.herokuapp.com/blogovi/${id}/objavikomentar`,
       body
     );
   }
   static getComments(id) {
-    return axios.get(`http://localhost:5000/blogovi/komentari/${id}`);
+    return axios.get(
+      `https://sambo-klub-neretva.herokuapp.com/blogovi/komentari/${id}`
+    );
   }
   static adminLogin(body) {
-    return axios.post(`http://localhost:5000/admin/login/uspesno`, body);
+    return axios.post(
+      `https://sambo-klub-neretva.herokuapp.com/admin/login/uspesno`,
+      body
+    );
   }
   static editBlogByid(id) {
-    return axios.get(`http://localhost:5000/blogovi/editujblog/${id}`);
+    return axios.get(
+      `https://sambo-klub-neretva.herokuapp.com/blogovi/editujblog/${id}`
+    );
   }
   static editComments(id) {
     return axios.get(
-      `http://localhost:5000/blogovi/editujblog/komentari/${id}`
+      `https://sambo-klub-neretva.herokuapp.com/blogovi/editujblog/komentari/${id}`
     );
   }
   static editUploadedBlog(id, body) {
     return axios.post(
-      `http://localhost:5000/blogovi/editujblog/azuriraj/${id}`,
+      `https://sambo-klub-neretva.herokuapp.com/blogovi/editujblog/azuriraj/${id}`,
       body
     );
   }
   static deleteBlog(id) {
     return axios.delete(
-      `http://localhost:5000/blogovi/editujblog/izbrisi/${id}`
+      `https://sambo-klub-neretva.herokuapp.com/blogovi/editujblog/izbrisi/${id}`
     );
   }
   static deleteComment(id, commentMail) {
     return axios.post(
-      `http://localhost:5000/blogovi/editujblog/izbrisi/komentar/${id}`,
+      `https://sambo-klub-neretva.herokuapp.com/blogovi/editujblog/izbrisi/komentar/${id}`,
       commentMail
     );
   }
   static existingComment(id) {
     return axios.get(
-      `http://localhost:5000/blogovi/editujblog/postojeci/${id}`
+      `https://sambo-klub-neretva.herokuapp.com/blogovi/editujblog/postojeci/${id}`
     );
   }
   static sendContact(body) {
-    return axios.post(`http://localhost:5000/kontakti/posalji`, body);
+    return axios.post(
+      `https://sambo-klub-neretva.herokuapp.com/kontakti/posalji`,
+      body
+    );
   }
   static getContacts() {
-    return axios.get(`http://localhost:5000/prikazikontakte/kontakti`);
+    return axios.get(
+      `https://sambo-klub-neretva.herokuapp.com/prikazikontakte/kontakti`
+    );
   }
   static deleteContact(id) {
     return axios.delete(
-      `http://localhost:5000/prikazikontakte/kontakti/izbrisi/${id}`
+      `https://sambo-klub-neretva.herokuapp.com/prikazikontakte/kontakti/izbrisi/${id}`
     );
   }
   static getImgName() {
-    return axios.get(`http://localhost:5000/galerija/uzmisliku`);
+    return axios.get(
+      `https://sambo-klub-neretva.herokuapp.com/galerija/uzmisliku`
+    );
   }
 }
 export default blogServices;
